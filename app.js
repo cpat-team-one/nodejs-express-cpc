1208073714f5a56efdded0375dad68923b4914b1
CHAD CRAMER <chadpcramer@gmail.com>
2020-01-14 16:20:24 -0800

    link email to github/commandline test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello Garage update techton plz");
+  res.send("Hello Garage update techton please");
 });
  
 module.exports.app = app;
