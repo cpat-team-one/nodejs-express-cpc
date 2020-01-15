0f52fcce0ba0c70b8ec7b899fa37c42263a4ed1b
CHAD CRAMER <chadcramer@CHADs-MacBook-Pro-2.local>
2020-01-14 16:05:37 -0800

    second try to update techton

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello Garage YOOOOOOOO");
+  res.send("Hello Garage update techton plz");
 });
  
 module.exports.app = app;
